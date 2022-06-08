a46ca5a04b30b560e11529a35d4228ac19a1c865
mamark90 <mamark90@gmail.com>
2022-06-08 17:24:19 -0400

    added rel="noreferrer" to Resume Component

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,16 +7,21 @@ const Resume = () => {
 				<Row>
 					<iframe
 						src="https://onedrive.live.com/embed?cid=E05F0A11E752D75B&amp;resid=E05F0A11E752D75B%213729&amp;authkey=ACW4wOICAsVntgE&amp;em=2&amp;wdStartOn=1"
+						rel="noreferrer"
 						width="476px"
 						height="288px"
 						frameborder="0"
 					>
 						This is an embedded{" "}
-						<a target="_blank" href="https://office.com">
+						<a target="_blank" href="https://office.com" rel="noreferrer">
 							Microsoft Office
 						</a>{" "}
 						document, powered by{" "}
-						<a target="_blank" href="https://office.com/webapps">
+						<a
+							target="_blank"
+							href="https://office.com/webapps"
+							rel="noreferrer"
+						>
 							Office
 						</a>
 						.
